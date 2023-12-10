334f577492dba96728de11283506c0957596e04d
Douglas A. Seifert <doug@dseifert.net>
2023-12-10 12:32:57 -0800

    implement part 2 day 5

--- 2023/05/5.rb
+++ 2023/05/5.rb
@@ -1,3 +1,10 @@
+class Range
+  def overlap?(other)
+    (self.begin <= other.begin && self.end > other.begin) ||
+      (self.begin > other.begin && other.end >= self.begin)
+  end
+end
+
 Almanac = Struct.new(:seeds, :maps) do 
   def initialize
     self.maps = []
@@ -18,6 +25,61 @@ Almanac = Struct.new(:seeds, :maps) do
     end
     #puts "LOCATION: #{val}"
     val
+  end
+
+  def offset_range(range, offset)
+    if range.exclude_end?
+      (range.begin+offset...range.end+offset)
+    else
+      (range.begin+offset..range.end+offset)
+    end
+
+  end
+
+  def min_loc(seed_ranges)
+    #puts seed_ranges.inspect
+    maps.each do |m|
+      #puts m.inspect
+      mapped_ranges = []
+      m.each do |range, offset|
+        new_ranges = []
+        unmapped_ranges = []
+        seed_ranges.each do |seed_range|
+          #puts "MAPPING #{seed_range.inspect} WITH MAP RANGE: #{range.inspect}, offset=#{offset}"
+          # four cases
+          if range.cover?(seed_range)
+            mapped_ranges << offset_range(seed_range, offset)
+            unmapped_ranges.delete(seed_range)
+          elsif seed_range.cover?(range)
+            new_ranges << (seed_range.begin...range.begin)
+            new_ranges << (range.end+1..seed_range.end)
+            mapped_ranges << offset_range((range.begin..range.end), offset)
+            unmapped_ranges.delete(seed_range)
+          elsif range.overlap?(seed_range)
+            if seed_range.begin < range.begin
+              mapped_ranges << offset_range((range.begin..seed_range.end), offset)
+              new_ranges << (seed_range.begin...range.begin)
+            else
+              mapped_ranges << offset_range((seed_range.begin..range.end), offset)
+              new_ranges << (range.end+1..seed_range.end)
+            end
+            unmapped_ranges.delete(seed_range)
+          else
+            unmapped_ranges << seed_range
+          end
+        end
+        #puts "After map: existing #{seed_ranges} new: #{new_ranges} mapped: #{mapped_ranges} unmapped: #{unmapped_ranges}"
+        seed_ranges = new_ranges + unmapped_ranges
+      end
+      #puts "Applied all maps: mapped: #{mapped_ranges}"
+      seed_ranges += mapped_ranges
+      #puts "Applied all maps: remaining: #{seed_ranges}"
+    end
+    min = 1e50
+    seed_ranges.each do |range|
+      min = [min, range.begin].min
+    end
+    min
   end
 end
 
@@ -50,4 +112,12 @@ def part1(a)
   a.seeds.map{|s| a.location(s) }.min
 end
 
+def part2(a)
+  seed_ranges = a.seeds.each_slice(2).map do |pair|
+    (pair[0]..(pair[0]+pair[1]-1))
+  end.sort_by{|r| r.begin}
+  a.min_loc(seed_ranges)
+end
+
 puts "Part 1: #{part1(a)}"
+puts "Part 2: #{part2(a)}"
--- 2023/README.md
+++ 2023/README.md
@@ -18,3 +18,7 @@ Notes, Blunders and Victories
 
 * Day 3: Took a while because I forgot to strip newlines off the input and thus counted as a part number
   any number at the end of the line, even if it wasn't otherwise next to a special character
+* Day 5: Part 2 took a while because I forgot the consider the case where the input range covered
+  the mapping range.  I had handled the other cases: where the mapping range covers the input range,
+  where they overlap and where they don't intersect. After writing things down on paper, was able to
+  finally get an implementation that worked.
