4a0f0c1bebb204501be34a4bd947b8093f699a73
Douglas A. Seifert <doug@dseifert.net>
2024-12-01 07:55:53 -0800

    2024 day 1

---
+++ common/solution.rb
@@ -0,0 +1,28 @@
+class Solution
+  def part1
+    raise "part1 solution not implemented"
+  end
+  def part2
+    raise "part2 solution not implemented"
+  end
+end
+
+if __FILE__ == $0
+  solution = Solution.new
+  err = 0
+  if ARGV.length == 0
+    err = 1
+    puts "ERROR: no arg provided"
+  elsif ARGV[0] == 'part1'
+    ARGV.shift
+    puts "Part 1: #{solution.part1}"
+  elsif ARGV[0] == 'part2'
+    ARGV.shift
+    puts "Part 2: #{solution.part2}"
+  else
+    puts "ERROR: Unknown arguments: #{ARGV.inspect}"
+  end
+  if err > 0
+    puts "Usage: ruby #{__FILE__} [part1|part2]"
+  end
+end
