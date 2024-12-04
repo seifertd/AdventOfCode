92456e24f401ecc7a82677b1de73e15b87f5b3e6
Douglas A. Seifert <doug@dseifert.net>
2024-12-03 20:42:34 -0800

    shorter impl for day 3

--- 2024/03/03.rb
+++ 2024/03/03.rb
@@ -1,58 +1,43 @@
+require 'strscan'
+
 class Solution
   def part1
-    instructions = ARGF.read
-    #mulre = /(?:[^a-z]|^)(mul\(\d+,\d+\))/
-    mulre = /(mul\(\d+,\d+\))/
+    instructions = StringScanner.new ARGF.read
+    mulre = /mul\((\d+),(\d+)\)/
     result = 0
-    while m = instructions.match(mulre)
-      multiplication = m[1].gsub('mul(','').gsub(')', '').split(',').map(&:to_i).inject(&:*)
-      result += multiplication
-      instructions = instructions[m.end(1)..-1]
+    while nums = instructions.scan_until(mulre)
+      result += instructions.captures.map(&:to_i).inject(&:*)
     end
     result
   end
+
   def part2
     enabled = true
-    mulre = /mul\(\d+,\d+\)/
-    dore = /do\(\)/
-    dontre = /don't\(\)/
-    instructions = ARGF.read
+    cmdre = /(mul)\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)/
     result = 0
-    while m = instructions.match(mulre)
+    instructions = StringScanner.new ARGF.read
+    while instructions.scan_until(cmdre)
+      cmd = instructions.captures
       if enabled
-        dontm = instructions.match(dontre)
-        debug "enabled? #{enabled}: FOUND MULT: #{m[0]}#{m.begin(0)} CLOSEST DONT: #{dontm.inspect}#{(dontm ? dontm.begin(0) : '')}\n"
-        if !dontm.nil? && dontm.begin(0) < m.begin(0)
-          debug "DISABLING\n"
+        if cmd[0] == 'mul'
+          result += cmd[1].to_i * cmd[2].to_i
+        elsif cmd[4] == "don't"
           enabled = false
-          instructions = instructions[m.end(0)..-1]
-          next
         end
       else
-        dom = instructions.match(dore)
-        debug "enabled? #{enabled}: FOUND MULT: #{m[0]}#{m.begin(0)} CLOSEST DO: #{dom.inspect}#{(dom ? dom.begin(0) : '')}\n"
-        if !dom.nil? && dom.begin(0) < m.begin(0)
-          debug "ENABLING\n"
+        if cmd[3] == 'do'
           enabled = true
-          instructions = instructions[dom.end(0)..-1]
-          next
-        else
-          instructions = instructions[m.end(0)..-1]
-          next
         end
       end
-      debug "COUNTING MULT\n"
-      multiplication = m[0].gsub('mul(','').gsub(')', '').split(',').map(&:to_i).inject(&:*)
-      result += multiplication
-      instructions = instructions[m.end(0)..-1]
+      debug "ENABLED: #{enabled} CMD: #{cmd.inspect} SUM: #{result}\n"
     end
     result
   end
+
   def debug(msg)
-    if ENV['DEBUG']
-      print(msg)
-    end
+    print(msg) if ENV['DEBUG']
   end
+
   def input
     ARGF.each_line do |line|
       line.chomp!
